1403f26973987993681bd4d2adb774264e5d3623
lara huang <lara1105huang@gmail.com>
2024-04-23 22:54:46 +0800

    新增axios 與測試獲取數據

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,37 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'App',
-  created(){
-    console.log(process.env.API_PATH, process.env.APP_ID);
-  }
+  data () {
+    return {
+      info:[],
+    }
+  },
+  created()  {
+    this.getWeatherApi();
+    this.getLists();
+  },
+
+  methods:{
+    getWeatherApi(){
+      const api =`${process.env.API_PATH}/data/2.5/forecast?q=Taipei,tw&APPID=${process.env.APP_ID}&lang=zh_tw&units=metric`;
+      axios
+      .get(api)
+        .then(res => (
+          this.info = res, console.log('info',this.info)
+        ))
+    },
+    getLists (){
+      const api ='https://api.coindesk.com/v1/bpi/currentprice.json';
+      this.$http.get(api).then((res) => {
+        console.log(res.data);
+      });
+
+    }
+  },
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
+axios.defaults.headers.post['Content-Type'] = ['application/json;charset=UTF-8']
 
 /* eslint-disable no-new */
-new Vue({
+export const app = new Vue({
   el: '#app',
   router,
   components: { App },
