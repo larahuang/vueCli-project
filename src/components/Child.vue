4973720f2276e8e669d2d3261a881878f0b60b42
lara huang <lara1105huang@gmail.com>
2024-04-23 23:45:34 +0800

    vueCli (vue2) prop

---
+++ src/components/Child.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h2>興趣：{{ interest }}</h2>
+    <ul>
+      <li v-for="item in lists" :key="item">
+        {{item.todo}}
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  props : {
+    interest : {
+        type : String,
+        require : true
+    },
+    lists:{
+      type : Object,
+      require : true
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+ <Child  :interest="interest" :lists="lists"/>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+  name: 'Home',
+  components: {
+    Child
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      interest:'游泳',
+      lists:[
+        {id:'001',todo:'去旅行'},
+        {id:'002',todo:'寫部落格'}
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
